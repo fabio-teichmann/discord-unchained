a1244dfe7d049673e92ee5fc3a5d52840b953fd4
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-08-02 09:34:11 +0300

    views

---
+++ discord_unchained/base/urls.py
@@ -0,0 +1,10 @@
+# App specific URLs
+from django.http import HttpResponse
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("room/", views.room, name="room"),
+]
