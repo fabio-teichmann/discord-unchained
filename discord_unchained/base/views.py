0958131d366352578a1812fa518f3749275061d4
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-08-10 10:33:24 +0200

    recent activity feed

--- discord_unchained/base/views.py
+++ discord_unchained/base/views.py
@@ -84,8 +84,14 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.all()
 
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
     return render(request, "base/home.html", context)
 
 
@@ -93,7 +99,7 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
 
     # Query children
-    messages = room.message_set.all().order_by("-created")
+    messages = room.message_set.all()  # .order_by("-created")
     # Many-to-many
     participants = room.participants.all()
 
